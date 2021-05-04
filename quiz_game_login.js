35d5dd45c57552e0818a4bdeb30918b818f058e0
Prithvi1573 <73021105+Prithvi1573@users.noreply.github.com>
2021-05-04 19:16:32 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,26 @@
+function addUser()
+{
+    player1=document.getElementById("player_1").value;
+    player2=document.getElementById("player_2").value;
+    localStorage.setItem("player1_name",player1);
+    localStorage.setItem("player2_name",player2);
+    window.location="quiz_game_page.html";
+
+}
+function gamepage()
+{
+    window.location="quiz_game_page.html"
+}
+function send()
+{
+    number1=document.getElementById("number1").value;
+    number2=document.getElementById("number2").value;
+    actual_answer=parseInt(number1)*parseInt(number2);
+    question_number="<h4>"+number1+"X"+number2+"</h4>";
+    input_box="<br>Answer:<input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_number+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";
+}
